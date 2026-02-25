4166cfee96c3a8550e1988393ddd254b382943d4
VOSDEV <AnthonyVos@65qps8.onmicrosoft.com>
2026-02-25 06:29:16 +0100

    Add files via upload

---
+++ Download-EnvoyConfig-Github.ps1
@@ -0,0 +1,17 @@
+﻿# Source variables
+$RepoOwner = "VOSDEV-web" # Fill in Github username
+$RepoName = "Envoy" # Fill in Repo name
+$PAT = "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" # Fill in Github PAT
+$FilePath = "Config.JSON" # Do not change!
+$DestinationFolder = "C:\Programdata\Envoy\" # Do not change!
+$Url = "https://api.github.com/repos/$RepoOwner/$RepoName/contents/$FilePath" # Do not change!
+
+# Authorization with PAT
+$Headers = @{
+    Authorization = "token $PAT"
+    Accept        = "application/vnd.github.v3.raw"
+}
+
+# Download
+Invoke-RestMethod -Uri $Url -Headers $Headers -OutFile "$DestinationFolder\$($FilePath.Split('/')[-1])"
+Write-Host "File downloaded to $DestinationFolder"
